3f23a290a6a01439d6865c82d6eebd23c0e81062
Stefan <simonovic520@gmail.com>
2026-09-19 23:15:47 +0200

    chore: upgrade Angular auth guards to v16

--- src/app/modules/core/auth/resolve.guard.ts
+++ src/app/modules/core/auth/resolve.guard.ts
@@ -1,19 +1,13 @@
 import { Training } from './../../training/models/training.model';
 import { UserService } from './../../shared/services/user.service';
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Resolve,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ResolveGuard implements Resolve<Training[]> {
+export class ResolveGuard  {
   constructor(private users: UserService) {}
   resolve() {
     // TODO: Give samo other data source
--- src/app/modules/core/auth/role.guard.ts
+++ src/app/modules/core/auth/role.guard.ts
@@ -1,18 +1,13 @@
 import { ToastService } from './../../shared/services/toast.service';
 import { AuthenticationService } from './authentication.service';
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RoleGuard implements CanActivate {
+export class RoleGuard  {
   constructor(private auth: AuthenticationService, private ts: ToastService) {}
   canActivate() {
     if (this.auth.isCorrectUserRole()) {
